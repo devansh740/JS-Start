8cc55bb57a03fd93db5b109c11ff07ec07533e92
Devansh Mishra <official.devansh91@gmail.com>
2024-10-29 20:47:30 +0530

    variables and constants (update)

--- Variables.js
+++ Variables.js
@@ -2,4 +2,4 @@ const accId= 142553
 let UserName= "Devansh"
 let accemail= "dev@gmail.com"
 let accpass= "951324"
-console.table({UserName, accId, accemail, accpass})
+console.table([UserName, accId, accemail, accpass])
